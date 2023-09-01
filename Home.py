d4d3720e3f49e4a1c9f2dd95fb89b7605c0063c7
Aryan <aryan.martinian@gmail.com>
2023-09-01 15:41:31 +0530

    Updated_file_2

--- Home.py
+++ Home.py
@@ -17,16 +17,14 @@ st.sidebar.image(logo)
 st.title("Streamlit for Geospatial Applications")
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/Aryanmartinian/Interactive_Map).
     """
 )
 st.header("Instructions")
 
 markdown="""
-1. For the [GitHub repository](https://github.com/Aryanmartinian) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
+1. For the [GitHub repository](https://github.com/Aryanmartinian) or [use it as a template](https://github.com/Aryanmartinian/Interactive_Map) for your own project.
 2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
 """
 
 st.markdown(markdown)
